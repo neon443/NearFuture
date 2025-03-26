1e36d1518497aa0e6a9aec055a23c49e4e125468
neon443 <69979447+neon443@users.noreply.github.com>
2025-03-26 14:08:10 +0000

    added tests

---
+++ NearFutureTests/NearFutureTests.swift
@@ -0,0 +1,16 @@
+//
+//  NearFutureTests.swift
+//  NearFutureTests
+//
+//  Created by neon443 on 26/03/2025.
+//
+
+import Testing
+
+struct NearFutureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
