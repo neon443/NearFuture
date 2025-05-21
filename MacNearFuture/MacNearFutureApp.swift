a64eb03cad0c640b81eb4a2bdf4c62dfe0dd6ca3
neon443 <69979447+neon443@users.noreply.github.com>
2025-05-21 14:45:04 +0100

    add Mac target and delet tests

---
+++ MacNearFuture/MacNearFutureApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacNearFutureApp.swift
+//  MacNearFuture
+//
+//  Created by neon443 on 21/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MacNearFutureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
